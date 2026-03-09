74f6214f8fe1e5888bd71565e7a10d6da11e472a
Park ji hoon <96074027+JiehoonPark@users.noreply.github.com>
2026-03-09 17:18:05 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.js
@@ -0,0 +1,11 @@
+function solution(numer1, denom1, numer2, denom2) {
+    const numer = numer1 * denom2 + numer2 * denom1;
+    const denom = denom1 * denom2;
+
+    function gcd(a, b) {
+        return b === 0 ? a : gcd(b, a % b);
+    }
+
+    const g = gcd(numer, denom);
+    return [numer / g, denom / g];
+}
